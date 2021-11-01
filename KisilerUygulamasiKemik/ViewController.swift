858edb8beea931385058a000f55467f97254b68a
fcyilmaz <fevzicanyilmazz@gmail.com>
2021-11-01 19:51:47 +0300

    Initial Commit

---
+++ KisilerUygulamasiKemik/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KisilerUygulamasiKemik
+//
+//  Created by Fevzi Can Yılmaz on 1.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
