9d01c6612ba38bdb29faa9a55953c937f41e92a4
fcyilmaz <fevzicanyilmazz@gmail.com>
2021-11-02 23:29:14 +0300

    first commit

---
+++ KisilerUygulamasiKemik/KisiHucreTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  KisiHucreTableViewCell.swift
+//  KisilerUygulamasiKemik
+//
+//  Created by Fevzi Can Yılmaz on 1.11.2021.
+//
+
+import UIKit
+
+class KisiHucreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
